14876b1855fd0f62178e512e735b069254a7c8be
estudiante <es@gmail.com>
2022-11-10 10:46:03 +0100

    tests depth acabado

--- src/test/java/PruebasDeph.java
+++ src/test/java/PruebasDeph.java
@@ -2,35 +2,36 @@ import com.binarytree.BinaryTree;
 
 import com.binarytree.Node;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
 public class PruebasDeph {
+    private BinaryTree<String> b;
 
+    @BeforeEach
+    public void setUp(){
+        b = new BinaryTree<String>("23");
+    }
 
     @Test
     public void testDepth1(){
-        BinaryTree<String> b = new BinaryTree<String>("23");
         Assertions.assertEquals(b.depth(), 0);
     }
     @Test
     public void testDepth2(){
-        BinaryTree<String> b = new BinaryTree<String>("23");
         b.insert("24", b.getRoot(), true);
         Assertions.assertEquals(b.depth(), 1);
     }
     @Test
     public void testDepth3(){
-        BinaryTree<String> b = new BinaryTree<String>("23");
-        b.insert("24", b.getRoot(), true);
-        Assertions.assertEquals(b.depth(b.search("24")), 0);
+        Node<String> n = b.insert("24", b.getRoot(), true);
+        Assertions.assertEquals(b.depth(n), 0);
     }
     @Test
     public void testDepth4(){
-        BinaryTree<String> b = new BinaryTree<String>("23");
-        b.insert("24", b.getRoot(), true);
-        b.insert("25", b.search("24"), true);
-        System.out.println(b);
-        Assertions.assertEquals(b.depth(b.search("24")), 1);
+        Node<String> n = b.insert("24", b.getRoot(), true);
+        b.insert("25", n, true);
+        Assertions.assertEquals(b.depth(n), 1);
     }
 }
